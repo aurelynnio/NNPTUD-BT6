42d6ff79ee65108d84304737541a87fd1955c7a0
Quoc Anh <quocanhlove199@gmail.com>
2026-03-13 14:31:28 +0700

    .

--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,75 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let userController = require('../controllers/users')
-let { RegisterValidator, handleResultValidator } = require('../utils/validatorHandler')
-let bcrypt = require('bcrypt')
-let jwt = require('jsonwebtoken')
-let {checkLogin} = require('../utils/authHandler')
+let userController = require("../controllers/users");
+let {
+  RegisterValidator,
+  ChangePasswordValidator,
+  handleResultValidator,
+} = require("../utils/validatorHandler");
+let bcrypt = require("bcrypt");
+let { signToken } = require("../utils/jwtHandler");
+let { checkLogin } = require("../utils/authHandler");
 /* GET home page. */
-router.post('/register', RegisterValidator, handleResultValidator, async function (req, res, next) {
+router.post(
+  "/register",
+  RegisterValidator,
+  handleResultValidator,
+  async function (req, res, next) {
     let newUser = userController.CreateAnUser(
-        req.body.username,
-        req.body.password,
-        req.body.email,
-        "69aa8360450df994c1ce6c4c"
+      req.body.username,
+      req.body.password,
+      req.body.email,
+      "69aa8360450df994c1ce6c4c",
     );
-    await newUser.save()
+    await newUser.save();
     res.send({
-        message: "dang ki thanh cong"
-    })
-});
-router.post('/login', async function (req, res, next) {
-    let { username, password } = req.body;
-    let getUser = await userController.FindByUsername(username);
-    if (!getUser) {
-        res.status(403).send("tai khoan khong ton tai")
-    } else {
-        if (getUser.lockTime && getUser.lockTime > Date.now()) {
-            res.status(403).send("tai khoan dang bi ban");
-            return;
-        }
-        if (bcrypt.compareSync(password, getUser.password)) {
-            await userController.SuccessLogin(getUser);
-            let token = jwt.sign({
-                id: getUser._id
-            },"secret",{
-                expiresIn:'30d'
-            })
-            res.send(token)
-        } else {
-            await userController.FailLogin(getUser);
-            res.status(403).send("thong tin dang nhap khong dung")
-        }
+      message: "dang ki thanh cong",
+    });
+  },
+);
+router.post("/login", async function (req, res, next) {
+  let { username, password } = req.body;
+  let getUser = await userController.FindByUsername(username);
+  if (!getUser) {
+    res.status(403).send("tai khoan khong ton tai");
+  } else {
+    if (getUser.lockTime && getUser.lockTime > Date.now()) {
+      res.status(403).send("tai khoan dang bi ban");
+      return;
     }
-
+    if (bcrypt.compareSync(password, getUser.password)) {
+      await userController.SuccessLogin(getUser);
+      let token = signToken({
+        id: getUser._id,
+      });
+      res.send(token);
+    } else {
+      await userController.FailLogin(getUser);
+      res.status(403).send("thong tin dang nhap khong dung");
+    }
+  }
 });
-router.get('/me',checkLogin,function(req,res,next){
-    res.send(req.user)
-})
+router.get("/me", checkLogin, function (req, res, next) {
+  res.send(req.user);
+});
 
+router.patch(
+  "/changepassword",
+  checkLogin,
+  ChangePasswordValidator,
+  handleResultValidator,
+  async function (req, res, next) {
+    let { oldpassword, newpassword } = req.body;
+    let user = req.user;
+    if (!bcrypt.compareSync(oldpassword, user.password)) {
+      res.status(400).send("oldpassword khong dung");
+      return;
+    }
+    await userController.ChangePassword(user, newpassword);
+    res.send({
+      message: "doi mat khau thanh cong",
+    });
+  },
+);
 
 module.exports = router;
